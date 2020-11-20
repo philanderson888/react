45745b360d00872a450ffd0b36c990e06b6a6b8a
philanderson888 <philanderson888@hotmail.com>
2020-11-20 09:53:59 +0000

    JWT refactor changes

--- StandaloneProjects/jwt/front-end/src/App.js
+++ StandaloneProjects/jwt/front-end/src/App.js
@@ -2,14 +2,15 @@ import React, {useState} from 'react';
 import axios from 'axios';
 const url = 'http://localhost:3001';
 function App() {
-  const [foods, setFoods] = useState([]);
+  const [foods, setFoods] = useState([{id:1,description:"description"},{id:2,description:"description2"}]);
+  const [myarray,setMyarray] = useState([1,2,3])
   const [jwt, setJwt] = useState('here is a jwt token')
   const getFoods = () => {
       axios.get(`${url}/foods`)
       .then((response)=> {
         console.log('food data',response.data.foods);
         console.log('one record',response.data.foods[0].description);
-        setFoods(response.data.foods[0].description);
+        setFoods(response.data.foods);
       });
   }
   const getJwt = () => {
@@ -19,16 +20,30 @@ function App() {
       console.log('token',response.data.token)
       setJwt(response.data.token)  
     })
-
   }  
+  const getItems = () => {
+    console.log('getting items')
+    const array = myarray
+    array.push(4)
+    console.log('array',array)
+    setMyarray(array)  
+  } 
   return (
     <div>
       <h2>Getting data from an API</h2>
       <button onClick={()=>getJwt()}>Get JWT</button>
       <button onClick={()=>getFoods()}>Get Foods</button>
+      <button onClick={()=>getItems()}>Get Items</button>
       <p>{jwt}</p>
       <ul> 
-        {foods}
+        {foods.map(food=>(
+          <li>{food.description}</li>
+        ))}
+      </ul>
+      <ul>
+        {myarray.map((item)=>(
+          <li>{item}</li>  
+        ))}
       </ul>
     </div>
   );
