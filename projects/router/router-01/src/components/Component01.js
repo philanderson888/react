f491b0904b9f32f821f81d6973f2172436eaecb6
philanderson888 <philanderson888@hotmail.com>
2024-10-15 08:35:11 +0100

    remove project router-01

--- projects/router/router-01/src/components/Component01.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-class Component01 extends React.Component {
-    render() {
-        return(<div>Component01</div>);  
-    }
-}
-export default Component01    
--- projects/router/router-01/src/components/Component02.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-class Component02 extends React.Component {
-    render() {
-        return(<div>Component02</div>);
-    }
-}
-export default Component02
